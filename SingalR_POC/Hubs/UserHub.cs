095010ae6589010bcbc1873f3b6195b79b022573
shravan <shravandrive08@gmail.com>
2024-03-01 17:13:36 -0600

    Add project files.

---
+++ SingalR_POC/Hubs/UserHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SingalR_POC.Hubs
+{
+    public class UserHub : Hub
+    {
+        public static int TotalViews { get; set; } = 0;
+        public async Task NewWindowLoaded()
+        {
+            TotalViews++;
+            //send update to all clients that total views have been updated
+            await Clients.All.SendAsync("updateTotalViews", TotalViews);
+        }
+    }
+}
